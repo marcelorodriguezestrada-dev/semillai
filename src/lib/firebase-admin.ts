4d74b2351d939f2946ba2b339ef98045829a5b32
Marcelo Rodriguez <marcelo.rodriguez.estrada@gmail.com>
2026-07-15 23:16:23 -0300

    fix: useAuth.ts → useAuth.tsx1

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,12 +1,18 @@
 import admin from "firebase-admin";
 
 if (!admin.apps.length) {
+  const privateKey = process.env.FIREBASE_PRIVATE_KEY
+    ? process.env.FIREBASE_PRIVATE_KEY.includes("\\n")
+      ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+      : process.env.FIREBASE_PRIVATE_KEY
+    : undefined;
+
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-    }),
+      privateKey,
+    } as admin.ServiceAccount),
   });
 }
 
